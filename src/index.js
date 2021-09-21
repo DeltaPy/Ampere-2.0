7cc3810291dc3b4da9c033c93b31953d456c7778
denis_dimchev <denis.dimchev@gmail.com>
2021-09-21 17:35:32 +0200

    Trying to figure out heroku bug #3

--- src/index.js
+++ src/index.js
@@ -15,17 +15,18 @@ const player = new Player(client, {
 });
 client.player = player;
 
-const functions = fs.readdirSync("./src/functions").filter(file => file.endsWith(".js"));
-const eventFiles = fs.readdirSync("./src/events").filter(file => file.endsWith(".js"));
-const commandFolders = fs.readdirSync("./src/commands");
+// const functions = fs.readdirSync("./src/functions").filter(file => file.endsWith(".js"));
+// const eventFiles = fs.readdirSync("./src/events").filter(file => file.endsWith(".js"));
+// const commandFolders = fs.readdirSync("./src/commands");
 
 
 (async () => {
-    for (file of functions) {
-        require(`./functions/${file}`)(client);
-    }
-    client.handlePlayerEvents(client.player);
+    // for (file of functions) {
+    //     require(`./functions/${file}`)(client);
+    // }
+    // client.handlePlayerEvents(client.player);
     // client.handleEvents(eventFiles, "./src/events");
     // client.handleCommands(commandFolders, "./src/commands");
+    console.log("Test")
     client.login(process.env.TOKEN);
 })();
